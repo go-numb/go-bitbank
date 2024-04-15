1c2287ee73ed9757cd33840ea84dc6b6df375df0
go-numb <gonumb66@gmail.com>
2024-04-16 03:25:13 +0900

    add order states

---
+++ v2/types/state.go
@@ -0,0 +1,10 @@
+package types
+
+const (
+	OrderStatusInactive            = "INACTIVE"
+	OrderStatusUnfilled            = "UNFILLED"
+	OrderStatusPartiallyFilled     = "PARTIALLY_FILLED"
+	OrderStatusFullyFilled         = "FULLY_FILLED"
+	OrderStatusCanceledUnfilled    = "CANCELED_UNFILLED"
+	OrderStatusCanceledPartialFill = "CANCELED_PARTIALLY_FILLED"
+)
